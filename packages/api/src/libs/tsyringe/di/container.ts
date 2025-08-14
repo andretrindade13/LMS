b69e537eb7b560263e0578bae689c2260ff14f64
André Luiz Mendonça Sanguinette Trindade - PrestServ <andre.trindade.prestserv@petrobras.com.br>
2025-08-14 13:48:27 -0300

    inject passwordHasher dependency for UserService

--- packages/api/src/libs/tsyringe/di/container.ts
+++ packages/api/src/libs/tsyringe/di/container.ts
@@ -3,13 +3,18 @@ import { container } from 'tsyringe';
 import { UserService } from '../../../services/user.service';
 import { UserRepositoryInMemory } from '../../../repositories/user/user.repositoryInMemory';
 import { IUserRepository } from '../../../repositories/user/user.interface';
+import { IPasswordHasher } from '../../../domains/services/passwordHasher.interface';
+import { BcryptPasswordHasher } from '../../crypto/bcryptPasswordHasher';
 
 container.register<IUserRepository>('UserRepository', {
   useClass: UserRepositoryInMemory,
 });
+container.register<IPasswordHasher>('PasswordHasher', {
+  useClass: BcryptPasswordHasher
+})
 
 // Cria instância manualmente
-const userServiceInstance = new UserService(container.resolve('UserRepository'));
+const userServiceInstance = new UserService(container.resolve('UserRepository'), container.resolve('PasswordHasher'));
 
 // Registra instância já pronta
 container.register<UserService>(UserService, { useValue: userServiceInstance });
